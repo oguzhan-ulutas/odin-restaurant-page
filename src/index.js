f2d53c63720d8206f38b4a1bd9bd144aca60b96e
Oguzhan Ulutas <mkoulutas@gmail.com>
2023-05-09 21:00:50 +0300

    Image loading with webpack works

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './style.css';
+import Icon from './img/restourant.jpg';
 
 console.log('helööö');
+
+const divContent = document.querySelector('#content');
+const img = document.createElement('img');
+img.src = Icon;
+console.log(Icon);
+divContent.appendChild(img);
