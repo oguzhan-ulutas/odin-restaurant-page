8f06a06bc0d144b58c4558e8edc406db2faeba80
Oguzhan Ulutas <mkoulutas@gmail.com>
2023-05-11 21:04:11 +0300

    Add main page styles

--- src/index.js
+++ src/index.js
@@ -3,3 +3,11 @@ import Icon from './img/restourant.jpg';
 import layoutCreator from './modules/layout';
 
 layoutCreator();
+
+const headerbuttons = document.querySelectorAll('.header-button');
+
+headerbuttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    console.log(button);
+  });
+});
