e74647e52eaff0e995c930bfdb9354fdcb244f33
Oguzhan Ulutas <mkoulutas@gmail.com>
2023-05-13 21:32:32 +0300

    Add menu tab styles

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,14 +1,29 @@
 import { elementCreator, clearDisplay, mainLayoutCreator } from './layout';
 import pancake from '../img/food2-chad-montano-eeqbbemH9-c-unsplash.jpg';
 import grilledKebab from '../img/food1-victoria-shes-UC0HZdUitWY-unsplash.jpg';
-import Salad from '../img/food3-eiliv-aceron-ZuIDLSz3XLg-unsplash.jpg';
+import salad from '../img/food3-eiliv-aceron-ZuIDLSz3XLg-unsplash.jpg';
 
 function foodContainerCreator(foodName, foodImage, foodDescription, foodPrize) {
-  elementCreator('.main-content', 'div', `${foodName}`); // Create Main food container
-  elementCreator(`.${foodName}`, 'img', `${foodName}-img`, `${foodImage}`); // Add food image
-  elementCreator(`.${foodName}`, 'div', `${foodName}-name`, `${foodName}`); // Add food name
-  elementCreator(`.${foodName}`, 'div', `${foodName}-description`, foodDescription); // Add food description
-  elementCreator(`.${foodName}`, 'div', `${foodName}-prize`, foodPrize); // Add food prize
+  elementCreator('.main-content', 'div', `${foodName.toLowerCase()}`); // Create Main food container
+  elementCreator(
+    `.${foodName.toLowerCase()}`,
+    'img',
+    `${foodName.toLowerCase()}-img`,
+    `${foodImage}`,
+  ); // Add food image
+  elementCreator(
+    `.${foodName.toLowerCase()}`,
+    'div',
+    `${foodName.toLowerCase()}-name`,
+    `${foodName}`,
+  ); // Add food name
+  elementCreator(
+    `.${foodName.toLowerCase()}`,
+    'div',
+    `${foodName.toLowerCase()}-description`,
+    foodDescription,
+  ); // Add food description
+  elementCreator(`.${foodName.toLowerCase()}`, 'div', `${foodName.toLowerCase()}-prize`, foodPrize); // Add food prize
 }
 
 // I will use same describtion for every food for now.
@@ -20,7 +35,7 @@ export default function menuPageCreator() {
   mainLayoutCreator();
   // Create menu header
   elementCreator('.main-content', 'div', 'menu-header', 'MENU');
-  foodContainerCreator('grilled-kebab', grilledKebab, foodDescription, '45$');
-  foodContainerCreator('salad', Salad, foodDescription, '35$');
-  foodContainerCreator('pancake', pancake, foodDescription, '20$');
+  foodContainerCreator('Kebab', grilledKebab, foodDescription, '45$');
+  foodContainerCreator('Salad', salad, foodDescription, '35$');
+  foodContainerCreator('Pancake', pancake, foodDescription, '20$');
 }
